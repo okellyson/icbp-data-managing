ffdbfd5a12ad8aa4e6486461c272c240c1dc25ce
okellyson <kellysonsilva99@gmail.com>
2023-11-21 04:48:17 -0300

    Cmon! Let's start this

---
+++ src/main/java/br/com/icbp/icbpdatamanaging/IcbpDataManagingApplication.java
@@ -0,0 +1,13 @@
+package br.com.icbp.icbpdatamanaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcbpDataManagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcbpDataManagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/icbp/icbpdatamanaging/IcbpDataManagingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icbp.icbpdatamanaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcbpDataManagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
